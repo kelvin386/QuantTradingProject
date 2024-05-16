f79e28505bbe2f8d6ffd338f7ac057506f13a0a7
kelvin386 <kelvin.wu.qk@gmail.com>
2024-05-16 23:01:45 +0100

    Fixed bug: OW/AFS now different opt.trades.

--- QuantTrading/BackTest.py
+++ QuantTrading/BackTest.py
@@ -1,8 +1,20 @@
 import numpy as np
 import pandas as pd
+def get_intended_impact(alphas_series, impact_summary, stock, model_type):
+    if model_type == 'linear': c = 1
+    else: c = 0.5
+
+    half_life = impact_summary.loc[stock]['half_life']
+    beta = np.log(2) / half_life
+    time_unit = 10
+    
+    intended_impacts = 1 / (1 + c) * (alphas_series - alphas_series.diff(1).shift(-1).fillna(0) / beta / time_unit)
+    intended_impacts.iloc[-1] = alphas_series.iloc[-1] # I_T^* = \alpha_T
+    
+    return intended_impacts
 
 def get_optimal_trades(monthly_scaling_factor, alphas_series, impact_summary,
-                       stock, date):
+                       stock, date, model_type):
     px_vol, ADV = monthly_scaling_factor.loc[stock, date]
 
     impact_coef = impact_summary.loc[stock]['beta_estimate']
@@ -11,14 +23,17 @@ def get_optimal_trades(monthly_scaling_factor, alphas_series, impact_summary,
     time_unit = 10
     decay_factor = np.exp(-beta * time_unit)
 
-
     # optimal trade
-    intended_impacts = 1/2 * (alphas_series - alphas_series.diff(1).shift(-1).fillna(0) / beta / time_unit)
-    intended_impacts.iloc[-1] = alphas_series.iloc[-1] # I_T^* = \alpha_T
+    intended_impacts = get_intended_impact(alphas_series, impact_summary, stock, model_type)
 
-    
-    optimal_trades = (beta * (alphas_series - alphas_series.diff(1).diff(1).shift(-1).fillna(0) / (beta ** 2 * time_unit ** 2)
-                    ) / (px_vol / ADV * impact_coef) / 2 * time_unit)
+    if model_type == 'linear':
+        # optimal_trades = beta * (alphas_series - alphas_series.diff(1).diff(1).shift(-1).fillna(0) / (beta ** 2 * time_unit ** 2)) \
+        #                   / ((px_vol / ADV * impact_coef) * 2) * time_unit 
+        optimal_trades = (beta * intended_impacts * time_unit + intended_impacts.diff(1).shift(-1).fillna(0)) \
+                          / (px_vol / ADV * impact_coef)
+    elif model_type == 'sqrt':
+        optimal_trades = (2 * intended_impacts * intended_impacts.diff(1).shift(-1).fillna(0) + beta * intended_impacts ** 2) \
+                          / (px_vol / ADV * impact_coef) * time_unit
 
     optimal_trades.iloc[0] = intended_impacts.iloc[0] / (px_vol / ADV * impact_coef) # I_0^* / lambda
     optimal_trades.iloc[-1] += (alphas_series.iloc[-1] - intended_impacts.iloc[-2] * decay_factor) / (px_vol / ADV * impact_coef)
